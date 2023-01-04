47c8d6efcf3aa00dec8449bf9e5769a69e655782
f4llen4ngel <f4llen4ngel@tutanota.com>
2023-01-04 21:44:22 +0300

    cringe fix

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from lib.database import service, models, base
 
 import asyncio
 
+
 async def main():
     await base.init_models()
-    session = await base.get_session()
+    session = base.get_session()
     await service.add_order(session, 1, "test")
     orders = await service.get_orders(session, 1)
     print(orders)
+
 
 asyncio.run(main())
